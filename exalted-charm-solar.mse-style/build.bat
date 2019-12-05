6b44b4e2fcfcf3b86e00c1820331ed31e43c0c95
Federico Serron <federico.serron@gmail.com>
2019-12-04 23:33:52 -0300

    Initial commit

---
+++ exalted-charm-solar.mse-style/build.bat
@@ -0,0 +1,7 @@
+cd ..
+
+cd ..
+
+mse --create-installer magic.mse-game magic-mana-small-exalted.mse-symbol-font	magic-mana-exalted.mse-symbol-font magic-watermarks.mse-include magic-exalted-common.mse-include
+
+ren "magic-future.mse-installer" "Magic - Futureshifted.mse-installer"
